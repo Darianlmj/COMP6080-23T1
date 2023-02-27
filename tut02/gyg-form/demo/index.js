a1653129965319d7a5844d661729dcb756d5a0c1
Darian Lee <darianlmj@gmail.com>
2023-02-27 21:17:03 +1100

    feat: add demo code for `gyg-nav`

---
+++ tut02/gyg-form/demo/index.js
@@ -0,0 +1,8 @@
+const inputBox = document.querySelector('#input-text');
+const submitButton = document.querySelector('#submit-button');
+const textArea = document.querySelector('#textarea');
+
+submitButton.addEventListener('submit', (event) => {
+  event.preventDefault();
+  inputBox.value = 
+})
