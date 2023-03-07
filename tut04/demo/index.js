004c5d124e14b0e683cd84b900a893a3bbe7205b
Darian Lee <darianlmj@gmail.com>
2023-03-07 13:48:42 +1100

    feat: add demo answer

--- tut04/demo/index.js
+++ tut04/demo/index.js
@@ -5,26 +5,54 @@
  */
 const getSpacexInfo = () => {
   fetch('https://api.spacexdata.com/v4/company', {
-    method: 'GET',
+    method: 'GET'
   })
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-    document.getElementById('info').textContent = data.name + data.founder;
-    document.getElementById('links').textContent = data['links']['twitter'];
-  });
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      
+      document.getElementById('info').textContent = `${data.name} ${data.founder} ${data.links.twitter}`
+    })
+    .catch(error => console.error(error))
 }
-
 
 /**
  * SpaceX's Rockets info
  */
 const getSpacexRockets = () => {
-  // TODO: Implement this function
+  fetch('https://api.spacexdata.com/v4/rockets', {
+    method: 'GET'
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    data.filter(rocket => {
+      return rocket.name !== "Falcon 1"
+    }).map(rocket => {
+      const rocketDiv = document.createElement('div');
+      rocketDiv.className = 'rocket';
+      rocketDiv.style.border = '1px solid black'
+      rocketDiv.margin = '10px'
+      rocketDiv.textContent = rocket.name;
+      
+      document.getElementById('rockets').appendChild(rocketDiv);
+      
+      rocket.flickr_images.forEach(image => {
+        const imageDiv = document.createElement('img');
+        imageDiv.src = image
+        imageDiv.style.width = '100px'
+        imageDiv.style.height = '100px'
+        imageDiv.alt = rocket.name
+        
+        document.getElementById('rockets').appendChild(imageDiv)
+      })
+    })
+  })
+  .catch(error => console.error(error))
 }
 
 
-getSpacexInfo();
-// getSpacexRockets();
+// getSpacexInfo();
+getSpacexRockets();
+
+
