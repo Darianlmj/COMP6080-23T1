e3ef33ed3c0bb7c08941a3800ffd9bab5cc03e03
Darian Lee <darianlmj@gmail.com>
2023-03-14 01:20:18 +1100

    chore(files): add `tut05` code

---
+++ tut05/demo/async_prac/index.js
@@ -0,0 +1,36 @@
+/**
+ * SpaceX's Rockets info
+ */
+const getSpacexRockets = () => {
+  fetch('https://api.spacexdata.com/v4/rockets', {
+    method: 'GET'
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    data.filter(rocket => {
+      return rocket.name !== "Falcon 1"
+    }).map(rocket => {
+      const rocketDiv = document.createElement('div');
+      rocketDiv.className = 'rocket';
+      rocketDiv.style.border = '1px solid black'
+      rocketDiv.margin = '10px'
+      rocketDiv.textContent = rocket.name;
+      
+      document.getElementById('rockets').appendChild(rocketDiv);
+      
+      rocket.flickr_images.forEach(image => {
+        const imageDiv = document.createElement('img');
+        imageDiv.src = image
+        imageDiv.style.width = '100px'
+        imageDiv.style.height = '100px'
+        imageDiv.alt = rocket.name
+        
+        document.getElementById('rockets').appendChild(imageDiv)
+      })
+    })
+  })
+  .catch(error => console.error(error))
+}
+
+getSpacexRockets();
